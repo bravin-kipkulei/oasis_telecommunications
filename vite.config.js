f4b79416efc81f651f382ce5e610192adb225051
Jack Ojango <ojangoh2@outlook.com>
2023-09-13 21:29:59 +0300

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
-import path from 'path'
-
-// Get absolute path to views
-const absoluteViews = path.resolve(__dirname, 'views')
-
-export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        views: absoluteViews 
-      }
-    } 
-  }
-})
+// vite.config.js
+export default {
+    build: {
+      outDir: 'dist', // Set this to your Vite build output directory
+    },
+  };
+  
