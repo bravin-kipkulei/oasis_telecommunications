b3e8ab4fb6d4584bb8d3c3de9901c8c0f159b060
Jack Ojango <ojangoh2@outlook.com>
2023-09-18 13:12:54 +0300

    Firebase Configuration

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyB6v7ZybUQe_Ywj2hyFKRmU_cfwlzJlV1w",
@@ -18,4 +15,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export { app };
+// Export the Firebase instance if needed
+export const firebaseInstance = app;
