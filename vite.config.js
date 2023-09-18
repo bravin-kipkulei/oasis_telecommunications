86bac82fdadf88b3256cef72d974ff4cdd30d0bf
Jack Ojango <ojangoh2@outlook.com>
2023-09-18 12:23:47 +0300

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,12 @@ export default defineConfig({
             .filter((file) => file.endsWith('.html'))
             .map((file) => [file.replace('.html', ''), resolve(__dirname, `views/${file}`)])
         ),
+        // Include all JS files in the 'views' directory
+        ...Object.fromEntries(
+          readdirSync(resolve(__dirname, 'views'))
+            .filter((file) => file.endsWith('.js'))
+            .map((file) => [file.replace('.js', ''), resolve(__dirname, `views/${file}`)])
+        ),
       },
     },
   },
