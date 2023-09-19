9f51b2032d25ff3c568f021990ed20d41ec21874
Jack Ojango <ojangoh2@outlook.com>
2023-09-19 15:16:25 +0300

    Home Page

--- views/home.js
+++ views/home.js
@@ -7,7 +7,7 @@ export const render = () => {
             <img src="hero-image.jpg" alt="Hero Image" class="img-fluid mb-4" style="max-width: 100%;">
             <h1 class="display-4">Welcome to Oasis Telecom</h1>
             <p class="lead">Your Trusted Partner in Telecommunication Solutions</p>
-            <a href="#contact" class="btn btn-primary btn-lg">Get Started</a>
+            <a href="/contact" class="btn btn-primary btn-lg">Get Started</a>
         </div>
     `;  
     return div;
